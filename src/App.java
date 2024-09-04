3b9c69be8dd44ee07204ce9c277a3396d8294e73
willy-hash <will.tech.code@gmail.com>
2024-09-03 22:34:45 -0500

    Initialized branches

--- src/App.java
+++ src/App.java
@@ -1,12 +1,14 @@
-import paquete_1.*;
-import paquete_2.*;
-import paquete_3.*;
+import FirstSection.*;
+import SecondSection.*;
+import ThirdSection.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-
-        Seccion_1 seccion1 = new Seccion_1();
-        seccion1.oneDocOne(5);
+        Section_1 section1 = new Section_1();
+        Section_2 section2 = new Section_2();
+        Section_3 section3 = new Section_3();
+        
+        section1.oneDocOne(5);
     }
 }
---
+++ src/SecondSection/Section_2.java
@@ -0,0 +1,12 @@
+package SecondSection;
+
+public class Section_2 {
+
+    
+
+}
+
+
+
+
+
