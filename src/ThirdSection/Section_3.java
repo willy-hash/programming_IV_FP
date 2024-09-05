1a17e2fb0b03031d01b174afc55bb8242c5397d2
willy-hash <will.tech.code@gmail.com>
2024-09-05 16:40:46 -0500

    2.23 - 2.24 done

--- src/ThirdSection/Section_3.java
+++ src/ThirdSection/Section_3.java
@@ -1,10 +1,13 @@
 package ThirdSection;
+import java.util.Scanner;
 
 public class Section_3 {
-    
+
     public static void main(String[] args) {
         //twoDottwentyOne();
-        twoDottwentyTwo();
+        //twoDottwentyThree();
+        twoDottwentyFour();
+
     }
 
     //2.21
@@ -33,7 +36,7 @@ public class Section_3 {
     //2.22
     public static void twoDottwentyTwo() {
         
-        int numbersList[] = new int[(int) (Math.random() * 5) + 1];
+        int numbersList[] = new int[(int) (Math.random() * 10) + 1];
 
         for(int i = 0; i < numbersList.length; i++) {
 
@@ -56,5 +59,55 @@ public class Section_3 {
         System.out.println("Min :" + min + ". Max:" + max);
     }
 
-    
-}
+    //2.23
+    public static void twoDottwentyThree() {
+
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber = (int) (Math.random() * 100);
+        int intNumber; // the number that the user will enter
+
+        System.out.println(randomNumber);
+
+        do{
+
+            System.out.print("Enter a whole number: ");
+            intNumber = scanner.nextInt();
+
+            if(intNumber > randomNumber ) {
+                System.out.println("The number is greater than the random number.");
+            }else if(intNumber < randomNumber) {
+                System.out.println("The number is less than the random number");
+            }
+
+        }while(intNumber != randomNumber); // if the number is not equal to the random number, the loop will continue
+
+        System.out.println("¡Congratulations! You found the number: " +randomNumber);
+
+        scanner.nextLine();
+        scanner.close(); // close sccanner
+    }
+
+    //2.24
+    public static void twoDottwentyFour(){
+        int numbersList[] = new int[10];
+        int evenNumber = 0;
+        int oddNumber = 0;
+
+        for(int i = 0; i < numbersList.length; i++) {
+
+            if ((int) (Math.random() * 10) >= 6){
+                numbersList[i] = (int) (Math.random() * -100);
+            }
+            else{
+                numbersList[i] = (int) (Math.random() * 100);
+            }
+        }
+
+        for(int number : numbersList){
+            if(number % 2 == 0) {evenNumber += number;} else{oddNumber += number;}
+        }
+
+        System.out.println("Sum of even numbers: " + evenNumber + " | Sum of odd numbers: "+ oddNumber);
+    }
+
+} 
