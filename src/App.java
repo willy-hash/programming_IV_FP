9acdc9dc968e9d5687e02d55a2e9e0b4748ceeb4
willy-hash <will.tech.code@gmail.com>
2024-09-05 11:54:40 -0500

    2.21 - 2.22 done

--- src/App.java
+++ src/App.java
@@ -1,12 +1,14 @@
-import FirstSection.*;
-import SecondSection.*;
-import ThirdSection.*;
+import FirstSection.Section_1;
+import SecondSection.Section_2;
+import ThirdSection.Section_3;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
 
         Section_1 section1 = new Section_1();
+        Section_2 section2 = new Section_2();
+        Section_3 section3 = new Section_3();
 
         section1.oneDocOne(5);
     }
--- src/ThirdSection/Section_3.java
+++ src/ThirdSection/Section_3.java
@@ -2,4 +2,59 @@ package ThirdSection;
 
 public class Section_3 {
     
+    public static void main(String[] args) {
+        //twoDottwentyOne();
+        twoDottwentyTwo();
+    }
+
+    //2.21
+    public static void twoDottwentyOne() {
+
+        int numbersList[] = new int[20];
+        int positiveNumbers = 0;
+        int negativeNumbers = 0;
+
+        for(int i = 0; i < 20; i++) {
+
+            if ((int) (Math.random() * 10) >= 6){
+                numbersList[i] = (int) (Math.random() * -1000);
+            }
+            else{
+                numbersList[i] = (int) (Math.random() * 1000);
+            }
+        }
+
+        for (int number : numbersList) {
+            if(number >= 0) {positiveNumbers++;} else {negativeNumbers++;}
+        }
+    System.out.println(positiveNumbers + " positive numbers and " + negativeNumbers + " negative numbers.");
+    }
+
+    //2.22
+    public static void twoDottwentyTwo() {
+        
+        int numbersList[] = new int[(int) (Math.random() * 5) + 1];
+
+        for(int i = 0; i < numbersList.length; i++) {
+
+            if ((int) (Math.random() * 10) >= 6){
+                numbersList[i] = (int) (Math.random() * -1000);
+            }
+            else{
+                numbersList[i] = (int) (Math.random() * 1000);
+            }
+        }
+
+        int max = numbersList[0];
+        int min = numbersList[0];
+
+        for(int i = 0; i<numbersList.length; i++) {
+            if(numbersList[i] > max) {max = numbersList[i];}
+            if(numbersList[i] < min) {min = numbersList[i];}
+        }
+        for(int number : numbersList){System.out.println(number);}
+        System.out.println("Min :" + min + ". Max:" + max);
+    }
+
+    
 }
